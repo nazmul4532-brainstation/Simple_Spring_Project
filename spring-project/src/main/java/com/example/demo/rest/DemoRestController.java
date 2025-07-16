cc673cd477e1e4c85a2827f5a4b4f555a499036f
Nazmul Hossain (IUT) <nazmulhossain@iut-dhaka.edu>
2025-07-16 07:49:42 +0600

    updates

--- spring-project/src/main/java/com/example/demo/rest/DemoRestController.java
+++ spring-project/src/main/java/com/example/demo/rest/DemoRestController.java
@@ -1,4 +1,4 @@
-package com.luv2code.demo.rest;
+package com.example.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- spring-project/src/main/java/com/example/demo/rest/LogController.java
+++ spring-project/src/main/java/com/example/demo/rest/LogController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
